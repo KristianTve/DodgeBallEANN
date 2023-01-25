91b5f0dcf8bc3b300b4bf5e77c3ba3876bbcde84
Kristian Tveiten <kristiantve@gmail.com>
2023-01-25 12:26:39 +0100

    Adapted the algorithm for 12 agents. Only seems to output actions for 6 of them. Need to include reward for each genome

--- UnityInterface.py
+++ UnityInterface.py
@@ -7,28 +7,19 @@ import numpy as np
 import atexit
 
 env = UE(seed=1, side_channels=[])
-
 env.reset()  # Resets the environment ready for the next simulation
-
 show_prints = True
-
 behavior_name = list(env.behavior_specs)[0]
 spec = env.behavior_specs[behavior_name]
-
 generation = 0
 
 if show_prints:
     print(f"Name of the behavior : {behavior_name}")
-
     print("Number of observations : ", len(spec.observation_specs))
     print(spec.observation_specs[0].observation_type)
 
-    # Decision_steps.obs prints out the raw observations of the agent.
-    # print("DECISION STEPS OBSERVATIONS")
-    # print(decision_steps.obs)  # Observations from the agent(s) state
-    # print("DECISION STEPS OBS END")
 
-
+# Handles the exit by closing the unity environment to avoid _communicator errors.
 def exit_handler():
     print("EXITING")
     env.close()
@@ -39,6 +30,7 @@ atexit.register(exit_handler)
 
 def run_agent(genomes, config):
     """
+    Population size is configured as 12 to suit the training environment!
     :param genomes: All the genomes in the current generation.
     :param config: Configuration files
     :return: Best genome from generation.
@@ -73,68 +65,62 @@ def run_agent(genomes, config):
     done = False  # For the tracked_agent
     total_reward = 0
 
+    # Agents:
+    agent_count = len(decision_steps.agent_id)  #
+    agents = list(decision_steps)  # Agent IDs
+    removed_agents = []
+
     while not done:
-        """
-        print("OBSERVATION 1")
-        print(decision_steps[agent_index].obs[0])   # Agent RayCast
-        print("OBSERVATION 2")
-        print(decision_steps[agent_index].obs[1])   # Ball RayCast
-        print("OBSERVATION 3")
-        print(decision_steps[agent_index].obs[2])   # Other Agents
-        print("OBSERVATION 4")
-        print(decision_steps[agent_index].obs[3])   # Rear Sensor
-        print("OBSERVATION 5")
-        print(decision_steps[agent_index].obs[4])   # Params Collected
-        print("OBSERVATION 6")  
-        print(decision_steps[agent_index].obs[5])   # Wall RayCast
-        """
+        # Store actions for each agent with 5 actions per agent (3 continuous and 2 discrete)
+        actions = np.zeros(shape=(23, 5))  # 23 in size because of the agent IDs going up to 22.
 
         # Concatenate all the data BESIDES number 3 (OtherAgentsData)
-        nn_input = np.concatenate((decision_steps[0].obs[0],
-                                   decision_steps[0].obs[1],
-                                   decision_steps[0].obs[3],
-                                   decision_steps[0].obs[4],
-                                   decision_steps[0].obs[5]))
+        nn_input = np.zeros(shape=(23, 364))  # 23 in size because of the agent IDs going up to 22.
 
-        action = policies[0].activate(nn_input)  # Works
-        action = np.array(action)  # Convert to ndarray
+        for agent in list(decision_steps):  # Collect observations from the agents requesting input
+            nn_input[agent] = np.concatenate((decision_steps[agent].obs[0],
+                                              decision_steps[agent].obs[1],
+                                              decision_steps[agent].obs[3],
+                                              decision_steps[agent].obs[4],
+                                              decision_steps[agent].obs[5]))
 
-        # Not elegant lol
-        action[4] = 1 if action[4] > 0 else 0       # DASH
-        action[5] = 1 if action[5] > 0 else 0       # THROW
+        if len(decision_steps) > 0:
+            for agent_index in agents:
+                if agent_index in decision_steps and agent_index < len(policies):
+                    action = policies[agent_index].activate(nn_input[agent_index])
+                    actions[agent_index] = action
 
-        #action[4] = 0
-        #action[5] = 1
+        # Clip discrete values to 0 or 1
+        for agent in agents:
+            actions[agent, 3] = 1 if actions[agent, 3] > 0 else 0   # Shoot
+            actions[agent, 4] = 1 if actions[agent, 4] > 0 else 0   # DASH
 
-        #print("ACTIONS CHOSEN:")
-        #print(action)
-
-        # Set the actions
-        action_tuple = ActionTuple(continuous=np.array([action[0:3]]), discrete=np.array([action[3:5]]))
-        #env.set_actions(behavior_name, action_tuple)
-
-        action = spec.action_spec.random_action(len(decision_steps))
-        action.add_continuous(np.array([[0.0, 0.0, 0.0]]))
-        env.set_actions(behavior_name, action)
+        # Set actions for each agent (convert from ndarray to ActionTuple)
+        if len(decision_steps.agent_id) != 0:
+            for agent in agents:
+                continuous_actions = [actions[agent, 0:3]]
+                discrete_actions = [actions[agent, 3:5]]
+                action_tuple = ActionTuple(discrete=np.array(discrete_actions), continuous=np.array(continuous_actions))
+                env.set_action_for_agent(behavior_name=behavior_name, agent_id=agent, action=action_tuple)
 
         # Move the simulation forward
         env.step()
-        # Get the new simulation results
 
+        # Get the new simulation results
         decision_steps, terminal_steps = env.get_steps(behavior_name)
+
+        # Collect reward
         if 0 in decision_steps:  # The agent requested a decision
             total_reward += decision_steps[0].reward
         if 0 in terminal_steps:  # The agent terminated its episode
             total_reward += terminal_steps[0].reward
             done = True
 
+        # Reward status
         sys.stdout.write("\rReward: %i" % total_reward)
         sys.stdout.flush()
 
-        if total_reward > 0.0:
-            print("REWARD MANs")
-            print(total_reward)
-
+    # Clean the environment for a new generation.
     env.reset()
     print("Finished generation")
 
@@ -155,3 +141,4 @@ if __name__ == "__main__":
 
     # Run NEAT
     best_genome = p.run(run_agent, 2)
+
